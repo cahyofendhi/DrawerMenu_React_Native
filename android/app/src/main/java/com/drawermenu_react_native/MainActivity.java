c814e9392e6debb0ac436d6a232a1593a0e76e0c
Bekti Cahyo <Bektic.22@gmail.com>
2019-07-12 10:40:49 +0700

    installed

--- android/app/src/main/java/com/drawermenu_react_native/MainActivity.java
+++ android/app/src/main/java/com/drawermenu_react_native/MainActivity.java
@@ -1,6 +1,9 @@
 package com.drawermenu_react_native;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +15,15 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "DrawerMenu_React_Native";
     }
+
+    @Override
+      protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+          @Override
+          protected ReactRootView createRootView() {
+           return new RNGestureHandlerEnabledRootView(MainActivity.this);
+          }
+        };
+      }
+
 }

8a8aa5c00f75fe55e227590fe3afb120aad23d6f
Bekti Cahyo <Bektic.22@gmail.com>
2019-07-15 09:02:10 +0700

    font asset

--- App.js
+++ App.js
@@ -1,44 +1,102 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
+import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
-  StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import MyDrawerNavigator from "./src/MyDrawerNavigator";
+import DrawerNavigation from "./src/module/navigation/DrawerNavigation";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <MyDrawerNavigator/>
-    </View>
+      <DrawerNavigation/>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 25,
-  },
-});
-
 export default App;
+
+
+
+// import React from 'react';
+// import { Ionicons } from 'react-native-vector-icons/Ionicons'
+// import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+// import { DrawerActions, createDrawerNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   menuOpen: {
+//     marginLeft: 10, 
+//     marginTop: 10
+//   }, 
+//   menuClose: {
+//     marginLeft: 14, 
+//     marginTop: 10
+//   }
+// });
+
+
+// const DrawerNavigator = createDrawerNavigator({
+//   Home: {
+//     screen: HomeScreen, 
+//     navigationOptions: ({ navigation }) => ({
+//       title: 'Home Screen', 
+//       drawerLabel: 'Home',
+//       drawerIcon: () => (
+//         <Ionicons name="ios-home" size={20} />
+//       )
+//     })
+//   },
+//   Profile: {
+//     screen: ProfileScreen, 
+//     navigationOptions: ({ navigation }) => ({
+//       title: 'Profile Screen', 
+//       drawerLabel: 'Profile',
+//       drawerIcon: () => (
+//         <Ionicons name="ios-person" size={20} />
+//       )
+//     })
+//   },
+//   Settings: {
+//     screen: SettingsScreen, 
+//     navigationOptions: ({ navigation }) => ({
+//       drawerIcon: () => (
+//         <Ionicons name="ios-settings" size={20} />
+//       )
+//     })
+//   }
+// });
+
+
+// const StackNavigator = createStackNavigator({
+//   DrawerNavigator: {
+//     screen: DrawerNavigator, 
+//     navigationOptions: ({ navigation }) => {
+//       const { state } = navigation;
+
+//       if(state.isDrawerOpen) {
+//         return {
+//           headerLeft: ({titleStyle}) => (
+//             <TouchableOpacity onPress={() => {navigation.dispatch(DrawerActions.toggleDrawer())}}>
+//               <Ionicons name="ios-close" style={styles.menuClose} size={36} color={titleStyle} />
+//             </TouchableOpacity>
+//           )
+//         }
+//       }
+//       else {
+//         return {
+//           headerLeft: ({titleStyle}) => (
+//             <TouchableOpacity onPress={() => {navigation.dispatch(DrawerActions.toggleDrawer())}}>
+//               <Ionicons name="ios-menu" style={styles.menuOpen} size={32} color={titleStyle} />
+//             </TouchableOpacity>
+//           )
+//         }
+//       }
+//     }
+//   }
+// })
+
+// export default App = createAppContainer(StackNavigator);
